fab8abd4ec870e8f6a77eee4298c255f4fbf4b4f
Wei Zhou <mrweiz03@gmail.com>
2025-03-16 17:35:40 +0100

    Cambios breves

--- include/usuari.hpp
+++ include/usuari.hpp
@@ -1,17 +1,29 @@
 
 #pragma once
 #include <string>
-
+#include "queryUsuari.hpp"
 class USUARI
 {
 
 private:
+
+	//cambiar a struct
+	// ALGO algo;
 	std::string ID;
 	std::string Nom;
 	std::string cognom;
 	std::string email;
 	std::string contrasenya;
-
+	/*
+	
+	Desde ForntEnd el usuario introduce datos.
+	Los datos son enviados a BackEnd, los recibimos y dichos datos son tratados en su respectivo metodo implementado
+	Tras terminar de tratar, se llama desde el mismo metodo, al archivo queryUsuari.hpp el cual tiene de rol
+	realizar llamadas a la BBDD
+	
+	
+	
+	*/
 public:
 	void registrarUsuari();
 	void iniciarSessio();
---
+++ pasarelaDB/queryUsuari.cpp
@@ -0,0 +1,1 @@
+#include usuari.hpp

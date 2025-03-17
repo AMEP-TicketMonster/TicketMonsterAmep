da17300054eef3ba2d67ada0f161a83baee2a5e9
Ruben <rubengorodri@gmail.com>
2025-03-17 16:47:47 +0100

    anotaciones clase

--- capaDomini/gestioUsuari/consultarUsuari.cpp
+++ capaDomini/gestioUsuari/consultarUsuari.cpp
@@ -1,3 +1,30 @@
+#include "usuari.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+// Constructor vacio
+Usuari::Usuari()
+{
+   
+}
+
+//constructor principal
+/*
+    to do:
+        - comprovar datos de entrada
+*/
+Usuari::Usuari(string idUsuari, string nom, string cognom, string email, string contrasenya) {
+    idUsuari = idUsuari;
+    nom = nom;
+    cognom = cognom;
+    email = email;
+    contrasenya = contrasenya;
+}
+
+
+
 #include "../include/usuari.hpp"
 #include "../pasarelaDB/queryUsuari.hpp"
 #include <iostream>
